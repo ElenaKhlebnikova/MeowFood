c91c8c67080e40b0ca805317a99e6de68c32789b
ElenaKhlebnikova <elenano555@gmail.com>
2022-10-29 16:54:03 +0300

    my husband is making me do this

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,14 +1,36 @@
 import styles from "./header.module.css";
 import mainPic from "../../assets/MainPic.jpg";
+import mainPicSmall from "../../assets/MainPic_small.jpg";
+import mainPicMedium from "../../assets/MainPic_medium.jpg";
+import mediaQuery from "./../media-query";
 import React from "react";
 
-const Header = () => (
-  <div>
-    <div className={styles.container}>
-      Delivering delicious meals to your door
+const Header = () => {
+  const smallDesktop = mediaQuery("(max-width: 900px, min-width: 500px)");
+  const mobile = mediaQuery("(max-width: 500px)");
+
+  const picture = () => {
+    if (mobile) {
+      return mainPicSmall;
+    }
+    if (smallDesktop) {
+      return mainPicMedium;
+    }
+
+    return mainPic;
+  };
+  return (
+    <div>
+      <div className={styles.container}>
+        Delivering delicious meals to your door
+      </div>
+      <img
+        className={styles.img}
+        src={picture()}
+        alt="Picture of tasty food"
+      ></img>
     </div>
-    <img className={styles.img} src={mainPic} alt="Picture of tasty food"></img>
-  </div>
-);
+  );
+};
 
 export default Header;
